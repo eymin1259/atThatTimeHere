51bdab4a491c0dc57b5bcee913543c19ea7f16da
yongmin <eymin1259@gamil.com>
2021-08-25 22:20:48 +0900

    initial setting

---
+++ atThatTimeHere/atThatTimeHere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  atThatTimeHere
+//
+//  Created by yongmin lee on 8/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
